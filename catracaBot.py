291fd0d4026b3253dae8f4e2760310ecc90a833a
Victor Serpa <victorscn@gmail.com>
2017-05-31 16:54:40 -0300

    setting up
    add avoid.txt and catracaBot.py

---
+++ catracaBot.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+import tweepy, time, sys, logging, json
+ 
+argfile = str(sys.argv[1])
+ 
+#enter the corresponding information from your Twitter application:
+// CONSUMER_KEY = ''#keep the quotes, replace this with your consumer key
+//CONSUMER_SECRET = ''#keep the quotes, replace this with your consumer secret key
+//ACCESS_KEY = ''#keep the quotes, replace this with your access token
+//ACCESS_SECRET = ''#keep the quotes, replace this with your access token secret
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+logging.getLogger("main").setLevel(logging.INFO)
+
+filename = open(argfile, 'r', encoding='utf-8')
+AVOID =filename.read().split()
+print(AVOID)
+filename.close()
+
+
+
+class PyStreamListener(tweepy.streaming.StreamListener):
+
+    def on_data(self, data):
+        tweet = json.loads(data)
+        try:
+            publish = True
+            for word in AVOID:
+                if word in tweet['text'].lower():
+                    logging.info("SKIPPED FOR {}".format(word))
+                    publish = False
+
+            print(tweet['user']['id_str'])
+            if tweet['user']['id_str'] !=  '18938394': #replace with target id
+           		publish = False
+
+            if publish:
+                api.retweet(tweet['id'])
+                logging.debug("RT: {}".format(tweet['text']))
+
+        except Exception as ex:
+            logging.error(ex)
+
+        return True
+
+    def on_error(self, status):
+        print (status)
+
+
+if __name__ == '__main__':
+    listener = PyStreamListener()
+    stream = tweepy.Stream(auth, listener)
+    stream.filter(follow=['18938394']) #replace with target id
+
